001e3db9773d4b6e4d79c60c19d0e267228979d9
yassen <yaseenseen9@gmail.com>
2024-04-17 19:58:28 +0530

    fix cors

--- index.js
+++ index.js
@@ -3,10 +3,18 @@ const app = express();
 const cors = require("cors");
 const clientRoute = require("./routes/clientRoute");
 const ConnectDb = require("./db");
-app.use(cors());
+
+// CORS configuration
+const corsOptions = {
+  origin: "https://blacktheme.onrender.com/",
+  optionsSuccessStatus: 200,
+};
+
+app.use(cors(corsOptions)); //
 app.use(express.json());
 app.use("/client", clientRoute);
 ConnectDb();
+
 app.listen(9090, () => {
-  console.log("listening to port 9090");
+  console.log("Listening on port 9090");
 });
